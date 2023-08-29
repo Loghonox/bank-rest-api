1118fd3a16ccb1c5d62a329c62e7c928d63cc407
loghonox <loghonos@gmail.com>
2023-08-29 22:51:44 +0100

    creating entity account and client and his relation model

--- src/main/java/com.gig.bankapi/model/Account.java
+++ src/main/java/com.gig.bankapi/model/Account.java
@@ -1,5 +1,72 @@
 package com.gig.bankapi.model;
-import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.validation.constraints.Size;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "account")
 
 public class Account {
+    @Id
+    private Long account_id;
+
+    @Column(nullable = false)
+    private String account_name;
+
+    @Column(nullable = false)
+    @Size(max = 31) //TODO test if there's a min
+    private String iban;
+
+    @Column(nullable = false)
+    private Long balance;
+
+    @Column(nullable = false)
+    private String currency;
+
+    @JoinColumn(name = "account_id", nullable = false)
+    @OneToOne(fetch = FetchType.LAZY)
+    private Client client;
+
+
+    public void setAccount_id(Long id) {
+        this.account_id = id;
+    }
+
+    public Long getAccount_id() {
+        return account_id;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getAccount_name() {
+        return account_name;
+    }
+
+    public void setAccount_name(String account_name) {
+        this.account_name = account_name;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
 }
--- src/main/java/com.gig.bankapi/model/Client.java
+++ src/main/java/com.gig.bankapi/model/Client.java
@@ -1,4 +1,35 @@
 package com.gig.bankapi.model;
 
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "client")
 public class Client {
+
+    @Id
+    private Long client_id;
+
+    @Column(nullable = false,name="first_name")
+    private String firstName;
+
+
+    @Column(nullable = false,name="last_name")
+    private String lastName;
+
+    @Column(nullable = false,name="birth_date")
+    private String birthDate;
+
+    @JoinColumn(name = "account_id", nullable = false)
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Account> lstAccount;
+
+    public void setClient_id(Long clientId) {
+        this.client_id = clientId;
+    }
+
+    public Long getClient_id() {
+        return client_id;
+    }
 }
